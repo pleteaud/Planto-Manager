d6cedc1af09226f83eb166c57d3c96875e7efb7e
Davo Pleteau <pleteaud@gmail.com>
2020-08-07 22:29:31 -0400

    Updated Alarm Object
    Alarm Object does not have the ability to initiate a command no more. It's responsibility is to simply initialize and set is time array and callback function

--- Code/alarm.h
+++ Code/alarm.h
@@ -9,45 +9,30 @@
 #define ALARM_H_
 
 /************************************************************************/
-/*							Includes/Constants	 	                */
+/*							Includes/Constants	 	                    */
 /************************************************************************/
 #include "stdint.h"
-#include "stdbool.h"
-#include "rtc.h"
-
-#define MAX_ALARM_TIME_UNITS	(TIME_UNITS_TOTAL-TIME_UNITS_DY)	 //Sec, Min, Hours, Day/Date
-
+#include "ds3231_regs_and_utils.h"
+#define MAX_ALARM_TIME_UNITS	(A2_MIN_ADDR-A1_SEC_ADDR)	 
+#define TOTAL_ALARM1_REGISTERS	(MAX_ALARM_TIME_UNITS) /* 4 */
+#define TOTAL_ALARM2_REGISTERS	(RTC_CTRL_ADDR-A2_MIN_ADDR) /* 3 */
 /************************************************************************/
 /*							Enums Definition		 	                */
 /************************************************************************/
-enum { ALARM_UNSET = 0, ALARM_SET };
-enum dt_dy_flag_e { NONE = 0, DATE, DAY};
-
-enum a1_match_option_e
+enum alarm_match_options_e 
 {
-	A1_MATCH_ONCE_PER_SEC = 0,
-	A1_MATCH_SEC,
-	A1_MATCH_MIN_SEC,
-	A1_MATCH_HR_MIN_SEC,
-	A1_MATCH_DT_HR_MIN_SEC,
-	A1_MATCH_DY_HR_MIN_SEC
+	A1_MATCH_ONCE_PER_SEC = 15, /* 1111 */
+	A1_MATCH_SEC = 14, //01110
+	A1_MATCH_MIN_SEC = 12, //01100
+	A1_MATCH_HR_MIN_SEC = 8, //01000 1
+	A1_MATCH_DT_HR_MIN_SEC = 0,//0 0000
+	A1_MATCH_DY_HR_MIN_SEC = 16,//1 0000
+	A2_MATCH_ONCE_PER_MIN = 7, //0111
+	A2_MATCH_MIN = 6, //0110
+	A2_MATCH_HR_MIN = 4,//0100
+	A2_MATCH_DT_HR_MIN = 0,//0000
+	A2_MATCH_DY_HR_MIN = 8 //1000
 };
-
-enum a2_match_option_e
-{
-	A2_MATCH_ONCE_PER_MIN = 0,
-	A2_MATCH_MIN,
-	A2_MATCH_HR_MIN,
-	A2_MATCH_DT_HR_MIN,
-	A2_MATCH_DY_HR_MIN
-};
-
-union alarm_match_u
-{
-	enum a1_match_option_e a1MF;
-	enum a2_match_option_e a2MF;
-};
-
 /************************************************************************/
 /*							Struct Definition							*/
 /************************************************************************/
@@ -60,17 +45,15 @@ typedef struct alarm_callback_s
 typedef struct alarm_s
 {
 	uint8_t time[MAX_ALARM_TIME_UNITS];
-	union alarm_match_u matchFlag;
-	uint8_t flag;
+	enum alarm_match_options_e matchFlag;
 	alarm_callback_t alarmCB;
 } alarm_t;
 
 /************************************************************************/
 /*							Public Interfaces    	                    */
 /************************************************************************/
-void alarmsInit(alarm_t *alarmP, uint8_t numAlarms);
-bool alarmSet(alarm_t *alarmP, uint8_t *time, enum dt_dy_flag_e dayDate);
+void alarmInit(alarm_t *alarmP);
+void alarmStoreTime(alarm_t *alarmP, uint8_t *time);
 void alarmSetCB(alarm_t *alarmP, void (*funcP)(void *objP), void *objP);
-void alarmsPoll(alarm_t *alarmList, uint8_t numAlarms);
-void alarmIrqHandle(alarm_t *alarmList, uint8_t numAlarms);
+void alarmExecuteCB(alarm_t *alarmP);
 #endif /* ALARM_H_ */
