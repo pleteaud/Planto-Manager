36414b96153b440a4781e9c62ac03b3fa432c06e
Davo Pleteau <pleteaud@gmail.com>
2020-08-14 03:12:44 -0400

    Create main.h
    Added a main.h to store all headers that's going to be used  in main.c

---
+++ Code/main.h
@@ -0,0 +1,24 @@
+/*
+ * main.h
+ *
+ * Created: 8/10/2020 1:46:45 AM
+ *  Author: plete
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "DHT11.h"
+#include "timer.h"
+#include "Moisture_Sensor.h"
+#include "rtc.h"
+#include "buffer.h"
+#include "cmd_proc.h"
+#include "i2cMasterControl.h"
+#include "LCD.h"
+
+
+
+
+#endif /* MAIN_H_ */
